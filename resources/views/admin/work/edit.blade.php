672012f957bc9d516d27488d36dfa5d349ea56ed
h-saitocm <1770hina@gmail.com>
2020-08-06 17:44:29 +0900

    【第6部 - viewとコントローラ (復習)】

---
+++ resources/views/admin/work/edit.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>MyBlog</title>
+</head>
+<body>
+    <h1>Myブログ編集画面</h1>
+</body>
+</html>
